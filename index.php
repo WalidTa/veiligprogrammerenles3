2bfab4a5ebaccf59b1e0bc1c590e5a5a5455cbf8
Walid <2111708@talnet.nl>
2020-12-04 23:20:06 +0100

    opd 4

--- index.php
+++ index.php
@@ -1,53 +1,23 @@
-<?php
-
-define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "");
-define("DB_NAME", "brokenauth");
-
-class DB{
-    protected $dbh;
-    protected $stmt;
-    protected $resultSet;
-
-    public function __construct(){
-            $this->dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASS);
-            $this->resultSet = [];
-    }
-
-    public function executeSQL($query){
-        $this->stmt = $this->dbh->prepare($query);
-
-        $result = $this->stmt->execute();
-
-        $row = $this->stmt->fetchAll();
-
-        if (!$result) {
-            die('<pre>Oops, Error execute query '. $query .'</pre><br><pre>'.'Result code: '. $result .'</pre>');
-        }
-
-        if(!empty($row)){
-            $this->resultSet = $row;
-            return $this->resultSet;
-        }
-
-    return $this->resultSet;
-}
-
-}
-
-
-
- ?>
-
+<!DOCTYPE html>
  <html>
-  <body>
-    <form method="POST" action="index.php" >
-      <input type="text" name="Username" placeholder="Username" required>
-      <input type="password" name="pswd" placeholder="Password" required>
-      <input type="submit"> <br>
-
-    </form>
-  </body>
-
- </html>
+  <head> 
+    <title> Login! </title>  
+  </head>
+    <body>
+    <h1> Login! </h1>
+      <h2> 
+        Vul je Email en Wachtwoord in
+      </h2>
+          <div class="inputFields">
+            <form method="POST" action="login.php" >
+              <input type="text" name="Email" placeholder="Fill in Email" required>
+                <br>
+                <br>
+                  <input type="password" name="password" placeholder="Fill in Password" required>
+                <br>
+                <br>
+              <input type="submit"> <br>          
+            </form>
+          <div>
+    </body>
+</html>
--- statements.sql
+++ statements.sql
@@ -4,4 +4,10 @@ create table user (
 id int(60) auto_increment primary key,
 email varchar(255) not NULL,
 password varchar(255) not null
+);
+
+ALTER TABLE user ADD COLUMN(
+	IP_adrress VARCHAR(80),
+	datum DATETIME DEFAULT CURRENT_TIMESTAMP
 )
+
